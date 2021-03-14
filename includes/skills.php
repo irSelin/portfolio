9ed84a521defe0504b320bafc7d69606a11d3ea8
Isabel Selin <is389@cornell.edu>
2021-03-13 21:39:58 -0500

    Update skills section

--- includes/skills.php
+++ includes/skills.php
@@ -1,24 +1,24 @@
 <section>
-  <h2>Coding Skills</h2>
+  <h2>Programming Languages</h2>
   <ul>
     <li>Java</li>
     <li>C</li>
     <li>C#</li>
     <li>Python</li>
     <li>JavaScript</li>
+    <li>HTML</li>
+    <li>CSS</li>
     <li>Elixir</li>
     <li>OCaml</li>
     <li>PostgreSQL</li>
     <li>R</li>
     <li>Box2D</li>
-    <li>Json</li>
-    <li>OUnit testing</li>
     <li>git</li>
   </ul>
 </section>
 
 <section>
-  <h2>Other Software Skills</h2>
+  <h2>Software Proficiencies</h2>
   <ul>
     <li>Unity</li>
     <li>Figma</li>
@@ -27,6 +27,18 @@
     <li>SketchUp</li>
     <li>Adobe Photoshop</li>
     <li>Adobe Illustrator</li>
+  </ul>
+</section>
+
+<section>
+  <h2>General Skills</h2>
+  <ul>
+    <li>Leadership</li>
+    <li>Communication and public speaking</li>
+    <li>Self-teaching new programming languages</li>
+    <li>User testing and user feedback analysis</li>
+    <li>Software testing and debugging</li>
+    <li>Illustration and animation</li>
   </ul>
 </section>
 <aside>In addition, I have experience in self-teaching new skills and languages: see my <a href="experience.html">Companio Internship</a></aside>
